e975972094338d152a7734ae1d35a04d18ec5af8
Bart0110 <Bart0110@users.noreply.github.com>
2017-02-20 16:37:05 +0100

    Remove locale

--- src/OVApi.js
+++ src/OVApi.js
@@ -3,10 +3,10 @@ import requestPromise from "request-promise";
 import x from "./throw-if-missing";
 
 
-const normalize_time = (t) => new Date((t / 1000 | 0) * 1000)
+const normalize_time = (t) => new Date((t / 1000 | 0) * 1000);
 const normalize_record_time = (data) => Object.assign(data, {
 	transactionDateTime: normalize_time(data.transactionDateTime)
-})
+});
 
 
 /**
@@ -20,9 +20,8 @@ export default class OVApi {
 	 * @param {String} password - Password used to access ov-chipkaart.nl. This field is required.
 	 * @param {String} [clientId=nmOIiEJO5khvtLBK9xad3UkkS8Ua] - Special client identifier used to access ov-chipkaart.nl.
 	 * @param {String} [clientSecret=FE8ef6bVBiyN0NeyUJ5VOWdelvQa] - Special client secret bound to the client identifier.
-	 * @param {String} [locale=nl-NL] - Locale specified to methods.
 	 */
-	constructor(username = x('Username'), password = x('Password'), clientId = "nmOIiEJO5khvtLBK9xad3UkkS8Ua", clientSecret = "FE8ef6bVBiyN0NeyUJ5VOWdelvQa", locale = 'nl-NL') {
+	constructor(username = x('Username'), password = x('Password'), clientId = "nmOIiEJO5khvtLBK9xad3UkkS8Ua", clientSecret = "FE8ef6bVBiyN0NeyUJ5VOWdelvQa") {
 		/**
 		 * The client identifier.
 		 * @type {String}
@@ -109,13 +108,6 @@ export default class OVApi {
 		 * @see {@link getTransactions}
 		 */
 		this._actionEndpoint = 'https://api2.ov-chipkaart.nl/femobilegateway/v1';
-
-		/**
-		 * Locale passed to TLS api.
-		 * @type {String}
-		 * @private
-		 */
-		this._locale = locale;
 
 		return this;
 	}
@@ -208,7 +200,6 @@ export default class OVApi {
 			uri: `${this._actionEndpoint}/${endpoint}`,
 			form: Object.assign({
 				authorizationToken: this._dataToken,
-				locale: this._locale
 			}, form)
 		}).then(body => {
 			const {c,o,e} = JSON.parse(body);
